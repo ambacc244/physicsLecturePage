1b20979e76cde6cdfa3ae054bf1eba7a5b0902a3
ambacc244 <leeye@oregonstate.edu>
2020-01-30 20:42:18 +0900

    create folders'

---
+++ physics/src/main/java/com/osu/lecture/user/User.java
@@ -0,0 +1,19 @@
+package com.osu.lecture.user;
+
+public class User {
+	private String userId;
+	private String userPw;
+
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getUserPw() {
+		return userPw;
+	}
+	public void setUserPw(String userPw) {
+		this.userPw = userPw;
+	}
+}
---
+++ physics/src/main/java/com/osu/lecture/user/dao/UserDao.java
@@ -0,0 +1,8 @@
+package com.osu.lecture.user.dao;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserDao {
+
+}
