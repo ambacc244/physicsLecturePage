6ab2007204cffbc57be97668b6b7aaeace5cc30c
Jaehyung You (Eric) <bapsajura@naver.com>
2020-02-23 12:28:25 +0900

    :+1:  Fix some misfuctional method
    * validate the right path to create.js
    * create.js file will validate user's input whether the inputs are emtpy or filled

--- physics/src/main/webapp/WEB-INF/views/create.jsp
+++ physics/src/main/webapp/WEB-INF/views/create.jsp
@@ -6,19 +6,19 @@
 <head>
 <meta charset="UTF-8">
 <title>Add Lecture</title>
-<script type="text/javascript" src="resources/js/create.js"></script>
+<script src="<c:url value="/resources/js/create.js"/>"></script>
 </head>
 <body>
 <%@ include file="partials/header.jsp" %>
     <h1>Add lecture page</h1>
 
     <form name="createLectureForm" class="create-lecture-form" action="${path}/lecture/mypage/add" method="POST">
-    	Title: <input type="text" name="lectureTitle" placeholder="lecture title">
-  		Description: <textarea name="lectureDesc" cols="30" rows="10" placeholder="lecture desc"></textarea>
-    	Link: <input type="text" name="lectureLink" placeholder="Link Address">
-  		Dates: <input type="date" name="lectureDate" placeholder="2020-01-01">
-		Time: <input type="time" name="lectureTime" placeholder="00:00:00">
-		<button type="submit" onclick="checkInputIsNull()">Submit</button>
+    	Title: <input type="text" class="titleFrom"  name="lectureTitle" placeholder="lecture title"> 
+  		Description: <textarea class="descFrom" name="lectureDesc" cols="30" rows="10" placeholder="lecture desc"></textarea>
+    	Link: <input type="text" class="linkForm" name="lectureLink" placeholder="Link Address">
+  		Dates: <input type="date" class="dateFrom" name="lectureDate" placeholder="2020-01-01">
+		Time: <input type="time" class="timeForm" name="lectureTime" placeholder="00:00:00">
+		<button type="submit" class="submitForm" onclick="return checkInputIsNull();">Submit</button>
     </form>
 </body>
 </html>
--- physics/src/main/webapp/resources/js/create.js
+++ physics/src/main/webapp/resources/js/create.js
@@ -1,34 +1,29 @@
+console.log("Create js file is called");
 function checkInputIsNull(){
-	//get values
-	var lectureId = document.getElementsByName('lectureId')[0].value;
-	var lectureTitle = document.getElementsByName('lectureTitle')[0].value;
-	var lectureDesc = document.getElementsByName('lectureDesc')[0].value;
-	var lectureLink = document.getElementsByName('lectureLink')[0].value;
-	var lectureDate = document.getElementsByName('lectureDate')[0].value;
-	var lectureTime = document.getElementsByName('lectureTime')[0].value;
-	//check empty value
-	if(lectureTitle == ""){
-		alert("Title을 입력하세요.");
-		return;
-	}
-	else if(lectureDesc == ""){
-		alert("Description을 입력하세요.");
-		return;
-	}
-	else if(lectureLink == ""){
-		alert("Link를 입력하세요.");
-		return;
-	}
-	else if(lectureDate == ""){
-		alert("Date를 입력하세요.");
-		return;
-	}
-	else if(lectureTime == ""){
-		alert("Time을 입력하세요.");
-		return;
+	var get_title_value = document.getElementsByClassName("titleFrom")[0];
+	var get_desc_value = document.getElementsByClassName("descFrom")[0];
+	var get_text_value = document.getElementsByClassName("linkForm")[0];
+	var get_date_value = document.getElementsByClassName("dateFrom")[0];
+	var get_time_value = document.getElementsByClassName("timeForm")[0];
+	
+	var input_values = [
+		get_title_value, get_desc_value, get_text_value, get_date_value, get_time_value
+	];
+
+	var reject_msg =[
+		"Title을 입력하세요",
+		"Description을 입력하세요",
+		"Link를 입력하세요",
+		"Date를 입력하세요",
+		"Time을 입력하세요"
+	]
+
+	for(var i=0; i< input_values.length; i++){
+		if(input_values[i].value == ''){
+			alert(reject_msg[i]);
+			return false;
+		}
 	}
 
-	if(confirm("강의를 추가 하시습니까?")){
-		document.createLectureForm.submit();
-	}
+    return confirm('강의를 새로 만드시겠어요?');;
 }
