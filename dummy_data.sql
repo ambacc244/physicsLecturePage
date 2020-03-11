1e6889c2d217f479d5cedd29ce162ad74628357a
Jaehyung You (Eric) <bapsajura@naver.com>
2020-03-11 11:53:40 +0900

    :construction_worker:  Update setup & dummy data query
    * THIS IS STILL WIP. DO NOT RUN YET!

---
+++ dummy_data.sql
@@ -0,0 +1,7 @@
+-- STILL Working in Progress!
+-- DUMMY DATA
+-- THIS QUERY MUST BE EXECUTED AFTER RUNNING THE SET-UP
+
+INSERT INTO lecture.user ('id', 'user_id', 'user_pw', 'user_name') VALUES ('1', 'admin', '0000', 'admin');
+
+INSERT INTO lecture.lecture (...) VALUES (...);
